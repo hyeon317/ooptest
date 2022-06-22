30e087debe5f3eb48708866dc68356983e00e506
수현 <tn_gus0317@naver.com>
2022-06-22 17:20:21 +0900

    초기 커밋.

---
+++ 0315/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _0329
+{
+    class Program
+    {
+        static void SumAvg(int[] abc)
+        {
+            int sum = 0;
+            for(int i = 0; i < abc.Length; i++)
+            {
+                sum += abc[i];
+            }
+            Console.WriteLine("합 : " + sum);
+            Console.WriteLine("평균 : " + sum / abc.Length);
+        }
+        static void Main(string[] args)
+        {
+            int[] abc = { 1, 2, 3, 4, 5 };
+            SumAvg(abc);
+        }
+    }
+}
